caa1f460e9c99e078b6b4ed1e9eb8dfc7dc3c622
artist78 <43592372+artist78@users.noreply.github.com>
2018-10-10 21:10:58 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 'strict mode';
-var bbCard = document.getElementById('Card');
+var Card = document.getElementById('Card');
 var flipper = document.getElementById('flipper');
 var isFlipped = /flipped/gi;
 
@@ -7,6 +7,6 @@ function flip(el) {
   el.className = isFlipped.test(el.className) ? '' : 'flipped';
 }
 
-bbCard.addEventListener("click", function () {
+Card.addEventListener("click", function () {
   flip(flipper);
 }, false);
